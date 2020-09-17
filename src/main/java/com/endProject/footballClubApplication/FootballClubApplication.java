f792ae334f62d6aca0dcfecf69bb3f7909a77189
Guntis Rubenis <guntis.rubenis@lspa.lv>
2020-09-17 17:25:20 +0300

    Second commit, added packages for models, repositories, sevices,controllers

--- src/main/java/com/endProject/footballClubApplication/FootballClubApplication.java
+++ src/main/java/com/endProject/footballClubApplication/FootballClubApplication.java
@@ -9,5 +9,5 @@ public class FootballClubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FootballClubApplication.class, args);
 	}
-
+	
 }
---
+++ src/main/java/com/endProject/footballClubApplication/MainPageController.java
@@ -0,0 +1,14 @@
+package com.endProject.footballClubApplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainPageController {
+	
+	@RequestMapping("/")
+	public String mainPage() {
+		return "index"; 
+	}
+
+}
