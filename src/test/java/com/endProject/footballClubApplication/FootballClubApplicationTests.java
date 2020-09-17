c6f06ad53647d31b311b180b906c57ef79958ff0
Guntis Rubenis <guntis.rubenis@lspa.lv>
2020-09-17 16:33:32 +0300

    First commit, blank project

---
+++ src/test/java/com/endProject/footballClubApplication/FootballClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endProject.footballClubApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
